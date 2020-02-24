fb4256f9fcd7558a2e46cc9f5d85427b2903ad60
Corey <hiben06@gmail.com>
2020-02-23 18:29:19 -0600

    Testing

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+all_posts = [
+    {
+        "title": "Post 1",
+        "content": "This is my content",
+        "author": "Corey"
+    },
+    {
+        "title": "Post 2",
+        "content": "This is my content 2"
+    }
+]
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/posts')
+def posts():
+    return render_template("posts.html", posts=all_posts)
 
 @app.route('/<string:user>')
 def user(user):
